8464dd4f5d607d2c15b5a85b312ca1b100107822
Hacker <78583783+DangerhackerRJ@users.noreply.github.com>
2021-04-07 22:14:04 +0530

    Update ForceSubscribe.py

--- LaylaRobot/modules/ForceSubscribe.py
+++ LaylaRobot/modules/ForceSubscribe.py
@@ -189,4 +189,4 @@ Note: Only creator of the group can setup me and i will not allow force subscrib
 Note: /FSub is an alias of /ForceSubscribe
  
 """
-__mod_name__ = "Subscribe"
+__mod_name__ = "📢 Force Subscribe"
